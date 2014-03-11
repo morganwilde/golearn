5ac720fcb1972b3adf405570495e5fcd6511f8a5
Morgan Wilde <wilde.morgan@gmail.com>
2014-03-11 17:01:05 +0200

    structs and pointers sorted out

---
+++ structs.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type person struct {
+    name    string
+    age     int
+}
+
+func main() {
+
+    fmt.Println(person{"Bob", 20})
+    fmt.Println(person{name: "Alice", age: 30})
+    fmt.Println(person{name: "Fred"})
+    fmt.Println(&person{name: "Ann", age: 40})
+
+    s := person{name: "Sean", age: 50}
+    fmt.Println(s.name)
+
+    sp := &s
+    fmt.Println(sp.age)
+
+    sp.age = 51
+    fmt.Printf("%p\n", &s)
+    fmt.Printf("%p\n", sp)
+    fmt.Println(&sp)
+    fmt.Println(s.age) // Structs are mutable
+}
