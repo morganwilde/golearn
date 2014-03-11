37442ed92f6f74d9eeb12a812d9f3d946e3459fb
Morgan Wilde <wilde.morgan@gmail.com>
2014-03-11 16:46:26 +0200

    fact() with closure

--- recursion.go
+++ recursion.go
@@ -9,6 +9,14 @@ func factorial(n int) int {
     return n * factorial(n - 1)
 }
 
+func fact() func() int {
+    i := 0
+    return func() int {
+        i += 1
+        return factorial(i)
+    }
+}
+
 func main() {
 
     fact1 := factorial(1)
@@ -18,4 +26,9 @@ func main() {
     fmt.Println("fact1:", fact1)
     fmt.Println("fact2:", fact2)
     fmt.Println("fact5:", fact5)
+
+    f := fact()
+    for i := 1; i < 10; i++ {
+        fmt.Println("f:", f())
+    }
 }
