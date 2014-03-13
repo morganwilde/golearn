559c22cf3c0b3f54a28e8ac0d156ca7a4af49bf7
Morgan Wilde <wilde.morgan@gmail.com>
2014-03-13 20:24:33 +0200

    fib test

--- goroutines.go
+++ goroutines.go
@@ -2,21 +2,33 @@ package main
 
 import "fmt"
 
-func  f(from string) {
-    for i := 0; i < 3; i++ {
-        fmt.Println(from, ":", i)
+func f(from string) {
+    var i int64 = 0
+
+    for i = 0; i < 40; i++ {
+        fmt.Println(from, "fib(", i, "):", fib(i))
+    }
+}
+
+func fib(n int64) int {
+    if n == 0 {
+        return 0
+    } else if n == 1 {
+        return 1
+    } else {
+        return fib(n-1) + fib(n-2)
     }
 }
 
 func main() {
 
-    f("direct")
+    go f("|||")
 
-    go f("goroutine")
+    go f("---")
 
     go func(msg string) {
         fmt.Println(msg)
-    }("going")
+    }("this should be three")
 
     var input string
     fmt.Scanln(&input)
